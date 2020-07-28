1e913bb12b66fe9fe982cdba1f232eb866d4d9b2
shreyamsingh <shreyamsingh34@gmail.com>
2020-07-28 16:09:57 -0400

    Refresh times

--- sentiment.py
+++ sentiment.py
@@ -46,8 +46,8 @@ def getProbs(text):
 def makeDF(df, query):
     api_key = os.environ['NEWS_API_KEY']
     today = date.today()
-    week_ago = today - timedelta(days=7)
-    url = (r'https://newsapi.org/v2/everything?q=' + query + " NOT pandemic NOT dies" + r'&from=' + str(week_ago) + r'&to=' + str(today) + r'&sortBy=popularity&excludeDomains=techcrunch.com,theverge.com,theinventory.com,gizmodo.com&apiKey=' + api_key)
+    week_ago = today - timedelta(days=6)
+    url = (r'https://newsapi.org/v2/everything?q=' + query + " NOT pandemic NOT dies NOT lawsuit" + r'&from=' + str(week_ago) + r'&to=' + str(today) + r'&sortBy=popularity&excludeDomains=techcrunch.com,theverge.com,theinventory.com,gizmodo.com&apiKey=' + api_key)
 
     response = requests.get(url).json()
     for article in response["articles"]:
